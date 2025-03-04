64892b69e870b15bb019ee20f14236aa90408c86
Josh Estus <josh.estus@gmail.com>
2025-03-04 10:20:10 -0500

    Add various java files

---
+++ MavenTest/src/main/java/org/joshestus/maventest/MavenTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.joshestus.maventest;
+
+/**
+ *
+ * @author joshestus
+ */
+public class MavenTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
